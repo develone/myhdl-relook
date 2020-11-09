e39fb18e12e0f61bfb156e6d586ccefebfcd3e3b
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-11-09 06:32:00 -0700

    python tb_fsm.py	creates testbench.vcd
    python convert 	creates framer_ctrl.v & tb_framer_ctrl.v
    
    gtkwave testbench.vcd

---
+++ fsm/convert.py
@@ -0,0 +1,20 @@
+import myhdl
+from myhdl import block, always, instance, Signal, ResetSignal, delay
+from fsm import framer_ctrl, t_state
+
+ACTIVE_LOW = 0
+
+@block
+def convert_fsm(hdl):
+	sof = Signal(bool(0))
+	sync_flag = Signal(bool(0))
+	clk = Signal(bool(0))
+	reset_n = ResetSignal(1, active=ACTIVE_LOW, isasync=True)
+	state = Signal(t_state.SEARCH)
+		
+	frame_ctrl_0 = framer_ctrl(sof, state, sync_flag, clk, reset_n)
+	frame_ctrl_0.convert(hdl=hdl)
+	
+
+		
+convert_fsm(hdl='Verilog')

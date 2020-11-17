93542eb8899d4c7fd51afdf213229e5638f143ce
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-11-17 08:09:24 -0700

    initial tests on relook

---
+++ fsm/tb_framer_ctrl.v
@@ -0,0 +1,33 @@
+module tb_framer_ctrl;
+
+wire sof;
+wire [1:0] state;
+reg sync_flag;
+reg clk;
+reg reset_n;
+
+initial begin
+    $from_myhdl(
+        sync_flag,
+        clk,
+        reset_n
+    );
+    $to_myhdl(
+        sof,
+        state
+    );
+end
+initial begin
+    $dumpfile("counter_top.vcd");
+    $dumpvars();
+end
+
+framer_ctrl dut(
+    sof,
+    state,
+    sync_flag,
+    clk,
+    reset_n
+);
+
+endmodule

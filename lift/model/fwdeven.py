8b189ec4aea9f07c270a1b35775282749abc84c9
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-11-20 03:54:35 -0700

    testing left sam right odd even

---
+++ lift/model/fwdeven.py
@@ -0,0 +1,15 @@
+l = 156
+s = 156
+r = 156
+a = l >>1
+b = r >>1 
+res = s - (a + b)
+print a,b,res
+ires = res + (a + b)
+print ires
+a = l + r + 2
+b = a >>2 
+res = s + b
+print a,b,res
+ires = res - b
+print ires

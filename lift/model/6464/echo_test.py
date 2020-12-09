fef495a974e031b4ade91a7f6acb22d649cbb2e0
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-12-08 19:33:30 -0700

    clk is now working

--- lift/model/6464/echo_test.py
+++ lift/model/6464/echo_test.py
@@ -9,21 +9,21 @@ A extremely simple example of co-simulation of MyHDL and Verilog code.
 import os
 from myhdl import Cosimulation, Simulation, Signal, delay, always, intbv, now
 
-def clk_driver(clk, period=10):
+def clk_driver(i_clk, period=10):
     ''' Clock driver '''
     @always(delay(period//2))
     def driver():
-        clk.next = not clk
+        i_clk.next = not i_clk
 
     return driver
      
-def echo(clk,uart_rx,uart_tx):
+def echo(i_clk,uart_rx,uart_tx):
 
 	
     
     ''' A Cosimulation object, used to simulate Verilog modules '''
     os.system('iverilog -o uu echotest.v echo_top.v')
-    return Cosimulation('vvp -m ./myhdl.vpi uu', clk=i_clk, uart_rx=i_uart_rx, uart_tx=o_uart_tx)
+    return Cosimulation('vvp -m ./myhdl.vpi uu', i_clk=i_clk, uart_rx=i_uart_rx, uart_tx=o_uart_tx)
 
  
  
@@ -36,8 +36,8 @@ uart_tx = Signal(0)
 i_uart_rx = Signal(0) 
 o_uart_tx = Signal(0)
 
-clk_driver_inst = clk_driver(clk)
-echotest_inst = echo(clk,uart_rx,uart_tx)
+clk_driver_inst = clk_driver(i_clk)
+echotest_inst = echo(i_clk,uart_rx,uart_tx)
  
 sim = Simulation(clk_driver_inst, echotest_inst)
 sim.run(200)

f50bf445a9d21d434f244ada69fd7085d66b2c1c
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-12-30 09:10:58 -0700

    moved top to main and modified top

--- transaction/main.py
+++ transaction/main.py
@@ -1,8 +1,8 @@
 from myhdl import *
-from main import main
-
+from RS232_Norbo import RS232_Module
+from RS232Programmer import RS232Programmer
 """
-yosys -l simple.log -p 'synth_ice40 -blif top.blif -json top.json' top.v
+yosys -l simple.log -p 'synth_ice40 -blif main.blif -json main.json' main.v
 === top ===
 
    Number of wires:                 42
@@ -23,16 +23,21 @@ yosys -l simple.log -p 'synth_ice40 -blif top.blif -json top.json' top.v
 from constsig import *
 
 @block
-def top(clk,iRX,oTX):
-	main_0 = main(clk,iRX,oTX)
+def main(clk,iRX,oTX):
+	rs232_instance=RS232_Module(clk,iRst,iRX,oTX, iData,WriteEnable,  \
+		oWrBuffer_full,oData,read_addr,rx_addr,Clkfrequenz=Clk_f,  \
+		Baudrate=BAUDRATE,RX_BUFFER_LENGTH=RX_BUFF_LEN)
+	programmer_inst=RS232Programmer(clk,iRst,programmer_enable,oInfobyte, \
+		dout,addr_out,we, \
+		oprog_Data_RS232,oprog_WriteEnable_RS232, iprog_WrBuffer_full_RS232,oData,read_addr,rx_addr)
 	return instances()
 
-def convert_top(hdl):
+def convert_main(hdl):
 	
-	top_0 = top(clk,iRX,oTX)
-	top_0.convert(hdl=hdl)
+	main_0 = main(clk,iRX,oTX)
+	main_0.convert(hdl=hdl)
 
 clk=Signal(bool(0))
 iRX=Signal(bool(1))
 oTX=Signal(bool(0))
-convert_top(hdl='Verilog')
+#convert_main(hdl='Verilog')

3f4dff78bc334b0820a846cd0572b81460117bce
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-01-17 09:06:08 -0700

    using iClk for testing with RS232_Norb

--- transaction/rs232loopback.py
+++ transaction/rs232loopback.py
@@ -2,24 +2,22 @@ from myhdl import *
  
 
 @block
-def rs232loopback(clock, iRX, oTX):
+def rs232loopback(iClk, iRX, oTX):
 	
-	@always(clock.posedge)
+	@always(iClk.posedge)
 	def logic():
 		iRX.next=oTX
 	return logic
 	
 def convert_rs232loopback(hdl):
-	clock=Signal(bool(0))
+	iClk=Signal(bool(0))
 	iRX=Signal(bool(1))
 	oTX=Signal(bool(1))
 	
-	i_clk=Signal(bool(0))
-	i_uart_rx=Signal(bool(0))
-	o_uart_tx=Signal(bool(0))
+
 	
 	#loopback_1 = rs232loopback(clock=i_clk, iRX=i_uart_rx, oTX=o_uart_tx)
-	loopback_1 = rs232loopback(clock, iRX, oTX)
+	loopback_1 = rs232loopback(iClk, iRX, oTX)
 	 
 	loopback_1.convert(hdl=hdl)
 

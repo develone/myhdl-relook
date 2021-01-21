ddd97448f710516f1a51529215d2ea115af2ba5a
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-01-21 16:16:24 -0700

    reset occrurs after 40 iClk and remains low for 20 iClk

--- rs232tests/pwruprst.py
+++ rs232tests/pwruprst.py
@@ -5,18 +5,24 @@ def pwruprst(iClk,iRst,pwrup):
 	
 	@always(iClk.posedge)
 	def logic2():
-		if((iRst==1)and(pwrup==1)):
+		
+		if((iRst==1)and(pwrup==40)):
 			iRst.next=0
-			pwrup.next=0
-		if(pwrup==0):
+			#pwrup.next=pwrup+1
+		else:
+			if(pwrup<=60):
+				pwrup.next = pwrup+1
+		if(pwrup==60):
 			iRst.next=1
+			
+		
 		
 	return logic2
 
 def convert_pwruprst(hdl):
 	iClk=Signal(bool(0))
 	iRst=Signal(bool(1))
-	pwrup=Signal(bool(1))
+	pwrup=Signal(intbv(0)[6:])
 	pwruprst_1 = pwruprst(iClk,iRst,pwrup)
 	 
 	pwruprst_1.convert(hdl=hdl)

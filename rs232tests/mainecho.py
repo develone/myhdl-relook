343ba39f248fee1db882227bacef7c58cc6b6656
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2021-02-01 10:14:32 -0700

    initial working version at 2MBaud

---
+++ rs232tests/mainecho.py
@@ -0,0 +1,143 @@
+from myhdl import *
+import RS232_Norbo
+import RS232Programmer
+#from RS232Programmer import RS232Programmer
+from pwruprst import pwruprst
+from rs232sig import *
+from updateecho import updateecho
+ 
+
+from pps import pps
+from rom import rom
+
+t_State = enum('IDLE','READ_INFO_DATA', 'INFO_DATA_HERE','WAIT_ONE_CLK', 'RECEIVE_IT')
+t_state1 = enum('IDLE','DEL0','DEL1','DEL2','DEL3','DEL4','DEL5')
+state1 = Signal(t_state1.IDLE)
+
+"""
+yosys -l simple.log -p 'synth_ice40 -blif mainecho.blif -json mainecho.json' mainecho.v
+nextpnr-ice40 --hx8k --pcf mainecho.pcf --json mainecho.json --asc mainecho.asc
+icetime -d hx8k -c 100 mainecho.asc
+icepack mainecho.asc mainecho.bin
+"""
+
+
+@block
+def mainecho(iClk,iRX,oTX):
+
+    pwruprst_inst = pwruprst(iClk,iRst,pwrup)
+    
+    pps0_inst=pps(iClk,ppscounter,sig)
+    
+    rom0_inst=rom(rom_dout,rom_addr,CONTENT)
+    
+     
+    
+    updateecho0_inst=updateecho(iClk,iRst,iData, WriteEnable,ldData,oWrBuffer_full,obusy)
+    
+    #transbuff0_inst = transbuff(iClk,iRst,WriteEnable,ldData,sig,rom_dout,rom_addr,CONTENT,oldData)
+
+    rs232_module_inst=RS232_Norbo.RS232_Module(iClk,iRst,iRX,oTX, \
+        iData,WriteEnable, oWrBuffer_full,oData,read_addr, \
+        rx_addr,Clkfrequenz=Clk_f, \
+        Baudrate=BAUDRATE,RX_BUFFER_LENGTH=RX_BUFF_LEN)
+    
+    programmer_inst=RS232Programmer.RS232Programmer(iClk,iRst, \
+        programmer_enable,oInfobyte,dout,addr_out,we, \
+        oprog_Data_RS232,oprog_WriteEnable_RS232, \
+        iprog_WrBuffer_full_RS232,oData,read_addr,rx_addr)
+    return instances()
+
+def convert_main(hdl):
+
+    mainecho_0 = mainecho(iClk,iRX,oTX)
+    mainecho_0.convert(hdl=hdl)
+
+@block
+def test_bench():
+
+    ##### Signal definitions #####
+
+    ##### Instanziate RS232 Module #####
+    main_0 = main(iClk,iRX,oTX)
+
+    interval = delay(10)
+    @always(interval)
+    def clk_gen():
+        iClk.next=not iClk
+
+    @always_comb
+    def rs232loopback():
+        iRX.next=oTX
+
+    @instance
+    def Monitor():
+        #print "\t\tPortC:",PORTC_OUT,"Binary:" ,bin(PORTC_OUT,WORD_SZ) ##TODO bin is not supported in simulation
+        while 1:
+            yield we
+            if we:
+                yield delay(0)
+                print "Data:\t",bin(dout),dout,"Addr:\t",addr_out
+
+    @instance
+    def Monitor2():
+        #### wait until data is received #####
+        count=0
+        currentPos=0
+        while 1:
+            yield iClk.posedge
+            yield delay(0)
+            count=count+1
+            if rx_addr!=read_addr:
+                count=0
+                #print "RXData:", oData, "\t\tBuffer Address:",read_addr
+                #assert oData==currentPos
+                if currentPos==255:
+                    currentPos=0
+                else:
+                    currentPos=currentPos+1
+                #read_addr.next=(read_addr+1)%RX_BUFF_LEN
+
+            #if Nothing is received for six possible complete RS232 transmissions
+            # (8+2) means 8 Bits + 1 Startbit + 1 Stopbit
+            if count>((Clk_f*1.0/BAUDRATE)*(8+2)*10):
+                break
+
+    def Write_to_rs232_send_buffer(value):
+        yield iClk.posedge
+        while oWrBuffer_full:
+            yield iClk.posedge
+        iData.next=value
+        if oWrBuffer_full:
+            print "Value:",value,"\tNot written, RS232 Transmittbuffer has indiciated to be allready full. (by oWrBuffer_full)"
+
+        WriteEnable.next=1
+        yield iClk.posedge
+        WriteEnable.next=0
+
+    def TestTransmitReceive(testARRAY):
+        #### Write some Bytes to the Sendbuffer #####
+        for data in testARRAY:
+            yield Write_to_rs232_send_buffer(data)
+
+        for i in range(int(Clk_f/BAUDRATE)*10*(RX_BUFF_LEN+3)):
+            yield iClk.posedge
+    @instance
+    def stimulus():
+
+        state1.next = t_state1.IDLE
+        
+        for i in range(1000000):
+            yield iClk.posedge
+        raise StopSimulation
+
+    #return  clk_gen,Monitor,stimulus,rs232_instance,programmer_inst,rs232loopback,Monitor2#,Monitor_oTX
+    return  clk_gen,Monitor,stimulus,main_0,rs232loopback,Monitor2#,Monitor_oTX
+
+
+convert_main(hdl='Verilog')
+"""
+tb = test_bench()
+tb.config_sim(trace=True)
+tb.run_sim()"""
+
---
+++ rs232tests/updateecho.py
@@ -0,0 +1,150 @@
+from myhdl import *
+from rs232sig import *
+
+t_state1 = enum('IDLE','DEL0','DEL1','DEL2','DEL3','DEL4','DEL5')
+state1 = Signal(t_state1.IDLE)
+
+from rom import rom
+
+
+
+    
+    
+@block
+def updateecho(iClk,iRst,iData, WriteEnable,ldData,oWrBuffer_full,obusy):
+ 
+
+    @always(iClk.posedge,iRst.negedge)
+    def rtl ():
+        if(iRst==0):
+            iData.next=0
+            ldData.next=0
+            WriteEnable.next=0
+            rom_addr.next=0
+            obusy.next=0
+            state1.next = t_state1.IDLE
+        else:
+            #WriteEnable.next=0
+            #iData.next=0
+            #ldData.next=0
+            #obusy.next=0
+            state1.next = t_state1.IDLE
+            if (state1 == t_state1.IDLE):
+                
+                if((not WriteEnable) and (not oWrBuffer_full)):
+                    if (oData != 0):
+                         
+                        ldData.next=oData
+                        obusy.next=1
+                        state1.next = t_state1.DEL0
+                    else:
+                        state1.next = t_state1.IDLE
+                    
+            elif (state1 == t_state1.DEL0):
+                 
+                state1.next = t_state1.DEL1
+            elif (state1 == t_state1.DEL1):
+                iData.next = ldData
+                WriteEnable.next=1
+                state1.next = t_state1.DEL2
+            elif(state1 == t_state1.DEL2):
+                WriteEnable.next=0
+                #if(rom_addr < 11):
+                #if(rom_addr < 18):
+                    #rom_addr.next=(rom_addr+1)
+                state1.next = t_state1.DEL3
+                #else:
+                    #rom_addr.next=0
+                    #state1.next = t_state1.IDLE
+            
+            elif (state1 == t_state1.DEL3):
+                WriteEnable.next=0
+                #state1.next = t_state1.DEL2
+                #obusy.next=1
+                #ldData.next=0
+                state1.next = t_state1.DEL4
+            
+            elif (state1 == t_state1.DEL4):
+                if(not sig):
+                     
+                    state1.next = t_state1.DEL4
+                else:
+                     
+                    state1.next = t_state1.DEL5
+                
+                 
+            else:
+                if(state1 == t_state1.DEL5):
+                    
+                    #WriteEnable.next=0
+                    obusy.next=0
+                    state1.next = t_state1.IDLE
+                
+ 
+    return rtl
+
+
+@block
+def testbench():
+  
+  updatebuff0_inst = updatebuff(iClk,iRst,iData, WriteEnable,ldData,oWrBuffer_full,obusy,rom_dout,rom_addr,CONTENT)
+  rom0_inst=rom(rom_dout,rom_addr,CONTENT) 
+  
+  @always(delay(10))
+  def clkgen():
+    iClk.next = not iClk
+    
+  @instance
+  def stimulus():
+    iRst.next=1
+    yield iClk.posedge
+    iRst.next=0
+    yield iClk.posedge
+    iRst.next=1
+    yield iClk.posedge
+    
+    state1.next = t_state1.IDLE
+    yield iClk.posedge
+    
+    if(rom_addr==9):
+        oWrBuffer_full.next = 1
+    for i in range(10):
+        print obusy
+        yield iClk.posedge
+    oWrBuffer_full.next = 0
+
+    
+    
+    yield iClk.posedge
+    
+    ldData.next = rom_dout
+    
+    for i in range(4):
+        print obusy
+        yield iClk.posedge
+
+    
+    #rom_addr.next=10
+    yield iClk.posedge
+    
+    ldData.next = rom_dout
+    
+    for i in range(4):
+        print obusy
+        yield iClk.posedge
+
+    for i in range(1000):
+      yield iClk.posedge
+    raise StopSimulation
+  return instances()
+
+def convert_updatebuff(hdl):
+    updatebuff_1 = updatebuff(iClk,iRst,iData, WriteEnable,ldData,oWrBuffer_full,obusy,rom_dout,rom_addr,CONTENT)
+    updatebuff_1.convert(hdl=hdl)
+    
+#onvert_updatebuff(hdl='Verilog')
+"""
+tb=testbench()
+tb.config_sim(trace=True)
+tb.run_sim()"""
+ 
